eb545174daa4b03c542a7d6dcddcd0277028ce64
StevaneRafael <stevanerafael@gmail.com>
2018-07-04 12:17:11 -0300

    2ª versao (Ate o terceiro DAO)

--- src/br/com/drogaria/dao/FabricanteDAO.java
+++ src/br/com/drogaria/dao/FabricanteDAO.java
@@ -2,12 +2,17 @@ package br.com.drogaria.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import br.com.drogaria.domain.Fabricante;
 import br.com.drogaria.factory.ConexaoFactory;
 
 public class FabricanteDAO {
+	private Fabricante retorno;
+
+
+
 	public void salvar(Fabricante f) throws SQLException {
 		StringBuilder sql = new StringBuilder();
 		sql.append("INSERT INTO fabricante ");
@@ -22,8 +27,64 @@ public class FabricanteDAO {
 		comando.executeUpdate();
 
 	}
+	
+	public void excluir(Fabricante f) throws SQLException{
+		StringBuilder sql = new StringBuilder();
+		sql.append("DELETE FROM fabricante ");
+		sql.append("WHERE codigo = ? ");
+		
+		Connection conexao = ConexaoFactory.conectar();
+		
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		comando.setLong(1, f.getCodigo());
+		
+		comando.executeUpdate();
+	}
+	
+	public void editar(Fabricante f)throws SQLException{
+		StringBuilder sql = new StringBuilder();
+		sql.append("UPDATE fabricante ");
+		sql.append("SET descricao = ? ");
+		sql.append("WHERE codigo = ? ");
+		
+		Connection conexao = ConexaoFactory.conectar();
+		
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		comando.setString(1, f.getDescricao());
+		comando.setLong(2, f.getCodigo());
+		
+		comando.executeUpdate();
+		
+	}
+	
+	public Fabricante buscarPorCodigo(Fabricante f) throws SQLException{
+		StringBuilder sql = new StringBuilder();
+		sql.append("SELECT codigo, descricao ");
+		sql.append("FROM fabricante ");
+		sql.append("WHERE codigo = ? ");
+		
+		Connection conexao = ConexaoFactory.conectar();
+		
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		comando.setLong(1, f.getCodigo());
+		
+		ResultSet resultado = comando.executeQuery();
+		
+		Fabricante retorno = null;
+		
+		if(resultado.next()) {
+			retorno = new Fabricante();
+			retorno.setCodigo(resultado.getLong("codigo"));
+			retorno.setDescricao(resultado.getString("descricao"));			
+		}
+		
+		return retorno;
+	}
+	
+	
 
 	public static void main(String[] args) {
+		/*
 		Fabricante f1 = new Fabricante();
 		f1.setDescricao("DESCRICAO 1");
 		
@@ -40,7 +101,63 @@ public class FabricanteDAO {
 			System.out.println("Ocorreu um erro ao tentar salvar um dos fabricantes!");
 			e.printStackTrace();
 		}
+		*/
 		
+		
+		/*
+		Fabricante f1 = new Fabricante();
+		f1.setCodigo(1L);
+		
+		Fabricante f2 = new Fabricante();
+		f2.setCodigo(2L);
+		
+		FabricanteDAO fdao = new FabricanteDAO();
+		
+		try {
+			fdao.excluir(f1);
+			fdao.excluir(f2);
+			System.out.println("Os fabricantes foram excluidos!");			
+		} catch (SQLException e) {
+			System.out.println("Ocorreu um erro ao tentar remover um dos fabricantes!");			
+			e.printStackTrace();
+		}
+		*/
+		
+		/*
+		Fabricante f1 = new Fabricante();
+		f1.setCodigo(1L);
+		f1.setDescricao("DESCRICAO 1");
+		
+		FabricanteDAO fdao = new FabricanteDAO();
+		try {
+			fdao.editar(f1);
+			System.out.println("Editado com sucesso!");
+		} catch (SQLException e) {
+			System.out.println("Falha ao Editar!");
+			e.printStackTrace();
+		}
+		*/
+		
+		Fabricante f1 = new Fabricante();
+		f1.setCodigo(3L);
+		
+		Fabricante f2 = new Fabricante();
+		f2.setCodigo(6L);
+		
+		FabricanteDAO fdao = new FabricanteDAO();
+		
+		try {
+			Fabricante f3 = fdao.buscarPorCodigo(f1);
+			Fabricante f4 = fdao.buscarPorCodigo(f2);
+			
+			System.out.println("Resultado 1: " + f3);
+			System.out.println("Resultado 2: " + f4);
+
+				
+		} catch (SQLException e) {
+			System.out.println("Ocorreu um erro ao tentar pesquisar um dos fabricantes!");
+			e.printStackTrace();
+		}
 		
 	}
 
