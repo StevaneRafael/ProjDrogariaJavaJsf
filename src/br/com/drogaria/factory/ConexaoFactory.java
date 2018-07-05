f94de98ba883ac2588358fbc16b9fe7b458850da
StevaneRafael <stevanerafael@gmail.com>
2018-07-04 23:21:32 -0300

    7ª versao (cadastro do fabricante parte 2)

--- src/br/com/drogaria/factory/ConexaoFactory.java
+++ src/br/com/drogaria/factory/ConexaoFactory.java
@@ -10,6 +10,7 @@ public class ConexaoFactory {
 	private static final String URL = "jdbc:mysql://localhost:3306/drogaria?useSSL=false";
 
 	public static Connection conectar() throws SQLException {
+		DriverManager.registerDriver(new com.mysql.jdbc.Driver());
 		Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
 		return conexao;
 	}
